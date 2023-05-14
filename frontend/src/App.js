f96929e9a39b8fbe2246ee08b58d5fccd5e34eab
Sahilkhan <sahilkhanahmadzai4@gmail.com>
2023-05-14 13:59:42 +0430

    installed some packages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,16 @@ import AddRecord from "./components/AddRecord";
 import RecordList from "./components/RecordList";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import EditRecord from "./components/EditRecord";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
 
 function App() {
   return (
     <div className="container pt-5">
       <BrowserRouter>
         <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
           <Route path="/" element={<RecordList />} />
           <Route path="/add" element={<AddRecord />} />
           <Route path="/edit/:id" element={<EditRecord />} />
